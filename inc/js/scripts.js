3625fc2d1a224ac274e6bff2eed0c85da0a79165
niti-miti <golubanton753@gmail.com>
2021-05-07 20:19:04 +0300

    create education section

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -15,9 +15,11 @@ window.onscroll = function() {
     scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if(scrolled > 1){
         $(".header").css({"box-shadow": "0px 5px 8px 1px rgba(0, 0, 0, 0.25)"})
+        $(".header").css({"border": "none"})
     }
     if(1 > scrolled){
         $(".header").css({"box-shadow": "none"})    
+        $(".header").css({"border-bottom": "1px solid #C4C4C4"})    
     }
 
 }
