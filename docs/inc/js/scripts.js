de98a6055719ca634515bf88b80f000e2a7297f8
niti-miti <golubanton753@gmail.com>
2021-05-08 13:17:43 +0300

    adaptive

--- docs/inc/js/scripts.js
+++ docs/inc/js/scripts.js
@@ -31,5 +31,17 @@ $(document).ready(function () {
         slidesToScroll: 1,
         nextArrow: '<span class="icon-arr"></span>',
         prevArrow: '<span class="icon-arr icon-arr--prev"></span>',
+         responsive: [
+            {
+            breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                },
+            breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
     })
 });
