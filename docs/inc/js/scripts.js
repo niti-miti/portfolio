335726eb3bd23ef44b79157c378633bf19328ac2
golub <golubanton753@gmail.com>
2021-06-24 08:50:35 +0300

    up

--- docs/inc/js/scripts.js
+++ docs/inc/js/scripts.js
@@ -1,18 +1,38 @@
-$(document).ready(function () {
-    $('.header-nav__link').on('click', function () {
-        var el = $(this);
-        var dest = el.attr('href');
-        if (dest !== undefined && dest !== '') {
-            $('html').animate(
-                {
-                    scrollTop: $(dest).offset().top,
-                },
-                500
-            );
-        }
-        return false;
-    });
+let headerLinks = document.querySelectorAll('.header-nav__link');
 
+let header = document.querySelector('.header');
+let headerWrapper = document.querySelector('div.header-wrapper');
+
+function checkPosition() {
+    var winPos = window.scrollY;
+    if (winPos >= 40) {
+        header.classList.add('scroll');
+        headerWrapper.classList.add('scroll');
+    } else {
+        header.classList.remove('scroll');
+        headerWrapper.classList.remove('scroll');
+    }
+}
+
+window.addEventListener('scroll', checkPosition);
+document.addEventListener('DOMContentLoaded', checkPosition);
+
+const headerCollectionLink = document.querySelectorAll('.header-nav__link');
+
+for (let headerLink of headerCollectionLink) {
+    headerLink.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        let id = headerLink.getAttribute('href').substr(1);
+
+        document.getElementById(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    });
+}
+
+$(document).ready(function () {
     $('.portfolio-slider').slick({
         infinite: false,
         variableWidth: true,
@@ -43,22 +63,3 @@ $(document).ready(function () {
         });
     }
 });
-
-let headerLinks = document.querySelectorAll('.header-nav__link');
-
-let header = document.querySelector('.header');
-let headerWrapper = document.querySelector('div.header-wrapper');
-
-function checkPosition() {
-    var winPos = window.scrollY;
-    if (winPos >= 40) {
-        header.classList.add('scroll');
-        headerWrapper.classList.add('scroll');
-    } else {
-        header.classList.remove('scroll');
-        headerWrapper.classList.remove('scroll');
-    }
-}
-
-window.addEventListener('scroll', checkPosition);
-document.addEventListener('DOMContentLoaded', checkPosition);
